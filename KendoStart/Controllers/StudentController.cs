1deefb2150422fac5c7034ef9f093df015e0f1bd
Pei <jzha471@aucklanduni.ac.nz>
2017-09-02 08:32:29 +1200

    5. Kendo and MVC Form Scratch

---
+++ KendoStart/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KendoStart.Models;
+
+namespace KendoStart.Controllers
+{
+    public class StudentController : Controller
+    {
+        private IC_DataBase _context ;
+
+        public StudentController()
+        {
+         
+            _context = new IC_DataBase();
+            _context.Configuration.ProxyCreationEnabled = false;
+        }
+
+
+        public ActionResult Index()
+        {
+            
+            return View(_context.Students.ToList());
+        }
+
+        public ActionResult Student_read()
+        {
+            var students = _context.Students;
+            return Json(students , JsonRequestBehavior.AllowGet);
+        }
+    }
+}
