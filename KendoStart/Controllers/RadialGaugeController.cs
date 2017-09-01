0f0f0eee505cc068d402464264056f597ad666b5
Pei <jzha471@aucklanduni.ac.nz>
2017-09-02 09:13:34 +1200

    5. Radial Gauge

---
+++ KendoStart/Controllers/RadialGaugeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoStart.Controllers
+{
+    public class RadialGaugeController : Controller
+    {
+        // GET: RadialGuage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
